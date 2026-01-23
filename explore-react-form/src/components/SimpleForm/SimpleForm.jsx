28c27a051d2bbe92aa8246b14f6304b162c52522
shakilahmad777 <shakilahmadpip3@gmail.com>
2026-01-23 17:26:08 +0600

    yhh i get simple form

--- explore-react-form/src/components/SimpleForm/SimpleForm.jsx
+++ explore-react-form/src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function SimpleForm() {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('hello hanibani', e.target.name.value)
+    }
   return (
     <div>
-        
+        <form onSubmit={handleSubmit}>
+            <input type="text" name='name' placeholder='Your Name' /> <br />
+            <input type="text" name='email' placeholder='Your Email' /> <br />
+            <input type="submit" value={'Submit'} />
+        </form>
     </div>
   )
 }
