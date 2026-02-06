3bcf743fef94a453150de23c58f8071a8be6ff3e
shakilahmad777 <shakilahmadpip3@gmail.com>
2026-02-07 04:30:22 +0600

    this added disk folder

--- book-vibes/src/components/Banner/Banner.jsx
+++ book-vibes/src/components/Banner/Banner.jsx
@@ -1,14 +1,14 @@
 import bannerImage from '../../../public/pngwing1.png'
 export default function Banner() {
     return (
-        <div className='md:flex md:justify-around p-10'>
+        <div className='flex flex-col-reverse md:flex-row md:justify-around p-10 bg-[#EEEEEE] rounded-[2rem] my-10 '>
             <div className="md:w-2/5 h-auto md:grid md:justify-between">
-                <h1 className='text-2xl md:text-4xl font-bold md:tracking-wide leading-[1.5]'>Books to freshen up your bookshelf</h1>
+                <h1 className='text-2xl lg:text-4xl font-bold md:tracking-wide leading-[1.5]'>Books to freshen up your bookshelf</h1>
                 <div className="grid sm:flex">
-                    <button className='btn my-10 md:my-0 bg-[#23BE0A] text-white'>View The List</button>
+                    <button className='btn my-10 md:my-0 bg-[#23BE0A] text-white md:p-10 md:text-xl'>View The List</button>
                 </div>
             </div>
-            <div className="">
+            <div className="flex justify-center">
                 <img src={bannerImage} alt="" />
             </div>
         </div>
